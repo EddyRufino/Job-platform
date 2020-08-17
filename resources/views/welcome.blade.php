bbfbfd089862d5020fef0afdca06b1ddf783bc2d
Eddy Rufino <eddyjaair@gmail.com>
2020-08-17 12:12:23 -0500

    xD

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel for home
                 </div>
 
                 <div class="links">
